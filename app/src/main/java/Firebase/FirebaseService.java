f8a592d6130d664a09fd5e4e84458631c219e698
RobertWais <wais.robert@uwlax.edu>
2018-10-01 12:54:46 -0500

    Added signup and signin

--- app/src/main/java/Firebase/FirebaseService.java
+++ app/src/main/java/Firebase/FirebaseService.java
@@ -1,5 +1,14 @@
 package Firebase;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 //import com.google.firebase.database.DatabaseReference;
@@ -15,9 +24,49 @@ public class FirebaseService {
     private FirebaseService(){}
 
 
-    public void login(){
+    //Pass Activity into command
+    //Possibly return value
+    public void login(String email, String password, final Activity activity){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                //SUCCESS
+                    Log.d("FIREBASE AUTH", "SUCCESS");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }else{
+                //NOT SUCCESS
+                    Log.d("FIREBASE AUTH", "ERROR");
+                    Toast.makeText(activity, "Authentication Failed",Toast.LENGTH_SHORT).show();;
+
+
+                }
+
+            }
+        });
 
     }
+
+    public void signUp(String email, String password, final Activity activity){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                //SUCCESS
+                    Log.d("FIREBASE AUTH", "SUCCESS");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }else{
+                    //NOT SUCCESS
+                    Log.d("FIREBASE AUTH", "ERROR");
+                    Toast.makeText(activity, "Sign Up Failed",Toast.LENGTH_SHORT).show();;
+                }
+            }
+        });
+    };
+
 
 
 }
