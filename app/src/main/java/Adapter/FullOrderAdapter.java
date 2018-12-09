8a6d901701f92c837c2d7194e176171705a46731
RobertWais <wais.robert@uwlax.edu>
2018-12-08 22:11:50 -0600

    ListView wrapped up

--- app/src/main/java/Adapter/FullOrderAdapter.java
+++ app/src/main/java/Adapter/FullOrderAdapter.java
@@ -52,24 +52,13 @@ public class FullOrderAdapter extends RecyclerView.Adapter<FullOrderAdapter.View
 
         public TextView name, total, shippingAddress, billingAddress, discountPrice, taxPrice, shippingPrice;
         public MyListView smallItems;
-        public ArrayList mobileArray;
-        public ArrayAdapter adapter;
+        public ArrayList<Item> mobileArray;
+        public CustomAdapter adapter;
         private List<Item> miniList;
 
         public ViewHolder(View view){
             super(view);
-
-            //Fix Later
-            // Array of strings...
-            mobileArray = new ArrayList<String>();
-            mobileArray.add("Hello");
-
-            //All the items in that purchase
-//            allItems = (RecyclerView)view.findViewById(R.id.fullOrderHistoryRecyclerView);
-
-            //MAYBE ADD LATER - IF ISSUES
-//            allItems.setHasFixedSize(true);
-//            allItems.setLayoutManager(new LinearLayoutManager(this));
+            mobileArray = new ArrayList<Item>();
 
             smallItems = (MyListView)  view.findViewById(R.id.listView);
 
@@ -82,7 +71,8 @@ public class FullOrderAdapter extends RecyclerView.Adapter<FullOrderAdapter.View
 
             miniList = new ArrayList<>();
 
-            adapter = new ArrayAdapter<>(context,R.layout.item_orderhistory,mobileArray);
+            adapter = new CustomAdapter(context,mobileArray);
+//            adapter = new ArrayAdapter<>(context,R.layout.item_orderhistory,mobileArray);
 
             smallItems.setAdapter(adapter);
         }
@@ -108,14 +98,11 @@ public class FullOrderAdapter extends RecyclerView.Adapter<FullOrderAdapter.View
         db = FirebaseDatabase.getInstance();
         database = db.getReference();
 
-
         if(mAuth.getCurrentUser()==null){
             cartRef = database.child("Guest").child("OrderHistory");
         }else{
             cartRef = database.child(mAuth.getCurrentUser().getUid()).child("OrderHistory");
         }
-
-
 
         holder.total.setText(order.getTotal().toString());
         holder.shippingAddress.setText(order.getShippingAddress());
@@ -123,48 +110,13 @@ public class FullOrderAdapter extends RecyclerView.Adapter<FullOrderAdapter.View
         holder.discountPrice.setText("Change later");
         holder.taxPrice.setText("Change later");
         holder.shippingPrice.setText("Change later");
-
-        cartRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Order allItems = dataSnapshot.getValue(Order.class);
-//                holder.mobileArray.add(newItem.getName().toString());
-                List<Item> items = allItems.getAllItems();
-                for (Item item: items) {
-                    if(item != null){
-                        String line = item.getName();
-                        if(s!=null){
-                            holder.mobileArray.add(line);
-                        }
-                    }
-
-                }
-
-
+        List<Item> items = order.getAllItems();
+        for (Item item: items) {
+            if(item != null){
+                holder.mobileArray.add(item);
                 holder.adapter.notifyDataSetChanged();
-//                holder.miniList.add(newItem);
             }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        }
     }
 
     @Override
